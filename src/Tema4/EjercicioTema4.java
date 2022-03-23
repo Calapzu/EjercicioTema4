d4fe7833cbe67426dfd28b2563e706075dbe8a22
Calapzu <yamsoncalapzu@gmail.com>
2022-03-23 18:40:08 -0500

    Se soluciona el ejercicio de la estructura de control if

---
+++ src/Tema4/EjercicioTema4.java
@@ -0,0 +1,14 @@
+package Tema4;
+
+public class EjercicioTema4 {
+    public static void main(String[] args) {
+       var numeroIf = 0;
+       if (numeroIf > 0){
+           System.out.println("POSITIVO");
+       }else if (numeroIf == 0){
+           System.out.println("El numero es 0");
+       }else{
+           System.out.println("NEGATIVO");
+       }
+    }
+}
